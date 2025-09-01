ba8ed5a80842af40c78209557e69c58b18dd4b01
Darshan S Gowda <vvce23cse0246@vvce.ac.in>
2025-09-02 00:26:33 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,10 @@
+num1=int (input("enter the first number "))
+num2=int(input("enter the second number "))
+addition=num1+num2
+subtraction=num2-num1
+multiplication=num1*num2
+division=num1/num2
+print ('Addition:' ,addition)
+print ('Subtraction: ',subtraction)
+print ('Multiplication:',multiplication)
+print ('Division: ',division)
